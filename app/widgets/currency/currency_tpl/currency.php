bcd01dddcc65dfe76da86b7242f99cbfd6f6bc87
freeone89 <51697296+NikolayMoshkin@users.noreply.github.com>
2020-02-25 16:25:55 +0300

    Delete some stuff

--- app/widgets/currency/currency_tpl/currency.php
+++
@@ -1,6 +0,0 @@
-<option value="" class="label"><?=$this->currency['code'];?></option>
-<?php foreach ($this->currencies as $key=>$value):?>
-    <?if ($key != $this->currency['code']):?>
-    <option value=<?=$key;?>><?=$key;?></option>
-    <?endif;?>
-<?endforeach;?>
